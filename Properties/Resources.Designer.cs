9b40e882e93bcc046c276262ebd1391a53709651
sbacorp <98741979+sbacorp@users.noreply.github.com>
2022-04-23 19:37:57 +0300

    added image

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace StudentsTransfer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Безымянный_1 {
+            get {
+                object obj = ResourceManager.GetObject("Безымянный-1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Безымянный_2 {
             get {
                 object obj = ResourceManager.GetObject("Безымянный-2", resourceCulture);
--- StudentForm.Designer.cs
+++ StudentForm.Designer.cs
@@ -34,18 +34,24 @@ namespace StudentsTransfer
             // 
             // MenuPanel
             // 
-            this.MenuPanel.Location = new System.Drawing.Point(12, 210);
+            this.MenuPanel.BackColor = System.Drawing.Color.Transparent;
+            this.MenuPanel.BackgroundImage = global::StudentsTransfer.Properties.Resources.Безымянный_1;
+            this.MenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuPanel.Font = new System.Drawing.Font("Gotham", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.MenuPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.MenuPanel.Name = "MenuPanel";
-            this.MenuPanel.Size = new System.Drawing.Size(200, 100);
+            this.MenuPanel.Size = new System.Drawing.Size(197, 585);
             this.MenuPanel.TabIndex = 0;
             // 
             // StudentForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 585);
             this.Controls.Add(this.MenuPanel);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Font = new System.Drawing.Font("Gotham", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "StudentForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "StudentForm";
